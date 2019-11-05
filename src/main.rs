dd4933f64d1d1964a002d2c24b4ec9d620b521d2
Sean Chen <skypemaster007@gmail.com>
2019-11-05 11:28:15 -0800

    Add parsing of select and insert keywords

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,55 @@
 use std::io;
+use std::process;
 use std::io::prelude::*;
 
 static PROMPT: &str = "db > ";
+
+enum MetaCommandResult {
+  Success,
+  Unrecognized
+}
+
+enum StatementType {
+  Insert,
+  Select
+}
+
+struct Statement {
+  typ: StatementType
+}
+
+#[derive(Debug)]
+enum ExecuteErr {
+  Reason(String)
+}
+
+type ExecuteResult<T> = Result<T, ExecuteErr>;
+
+fn do_meta_command(command: &str) -> MetaCommandResult {
+  match command.trim() {
+    ".exit" => process::exit(0),
+    _ => MetaCommandResult::Unrecognized
+  }
+}
+
+fn prepare_statement(command: &str) -> Option<Statement> {
+  if command.starts_with("insert") {
+    Some(Statement { typ: StatementType::Insert })
+  } else if command.starts_with("select") {
+    Some(Statement { typ: StatementType::Select })
+  } else {
+    None
+  }
+}
+
+// fn execute_statement(s: &Statement) -> ExecuteResult<()> {
+//   match s.typ {
+//     StatementType::Insert => println!("Performing INSERT"),
+//     StatementType::Select => println!("Performing SELECT")
+//   }
+
+//   Ok(())
+// }
 
 fn main() {
   loop {
@@ -11,10 +59,23 @@ fn main() {
     let mut command = String::new();
     io::stdin().read_line(&mut command).expect("failed to read from stdin");
 
-    if command.trim() == ".exit" {
-      break;
+    if command.starts_with('.') {
+      match do_meta_command(&command) {
+        MetaCommandResult::Success => continue,
+        MetaCommandResult::Unrecognized => {
+          print!("Unrecognized command: {}", command);
+          continue;
+        } 
+      }
+    }
+
+    if let Some(s) = prepare_statement(&command) {
+      match s.typ {
+        StatementType::Insert => println!("Performing INSERT"),
+        StatementType::Select => println!("Performing SELECT")
+      }
     } else {
-      println!("Unrecognized command: {}", command);
+      print!("Unrecognized keyword at start of {}", command);
     }
   }
 }
